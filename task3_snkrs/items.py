46b7d6a7667fc2271d677669c8bb3945ea2ac9f8
Arslan Shakar <14051598-039@uog.edu.pk>
2019-08-09 00:13:03 +0500

    Revisions Completed

--- task3_snkrs/items.py
+++ task3_snkrs/items.py
@@ -1,16 +1,12 @@
 import scrapy
 
 
-class Task3SnkrsItem(scrapy.Item):
-    product_id = scrapy.Field()
+class SnkrsItem(scrapy.Item):
+    item_id = scrapy.Field()
     name = scrapy.Field()
     brand = scrapy.Field()
     description = scrapy.Field()
     availability = scrapy.Field()
     currency = scrapy.Field()
     image_urls = scrapy.Field()
-    old_prices = scrapy.Field()
-    new_price = scrapy.Field()
-    sizes = scrapy.Field()
     skus = scrapy.Field()
-    sku_id = scrapy.Field()
--- task3_snkrs/spiders/snkrs_spider.py
+++ task3_snkrs/spiders/snkrs_spider.py
@@ -1,57 +1,86 @@
 import scrapy
-from task3_snkrs.items import Task3SnkrsItem
+from task3_snkrs.items import SnkrsItem
 
 
 class SnkrsSpider(scrapy.Spider):
     name = 'snkrs'
+    response = None
+    start_urls = [
+        'https://www.snkrs.com/'
+    ]
 
-    def start_requests(self):
-        url = 'https://www.snkrs.com/'
-        yield scrapy.Request(url, self.parse_categories)
+    def parse(self, response):
+        urls_s = response.css('ul.idparent-63 li a')
+        return [response.follow(url_s, callback=self.parse_pages) for url_s in urls_s]
 
-    def parse_categories(self, response):
-        for a in response.css('ul.idparent-63 li a'):
-            yield response.follow(a, self.product_pagination)
+    def parse_pages(self, response):
+        yield from self.parse_product_urls(response)
 
-    def product_pagination(self, response):
-        for a in response.css('a.product_img_link'):
-            yield response.follow(a, self.parse_product)
-
-        for a in response.css('li.pagination_next a'):
-            yield response.follow(a, self.product_pagination)
+        yield from [response.follow(url_s, callback=self.parse_pages) for url_s in response.css('li.pagination_next a')]
 
     def parse_product(self, response):
-        prod = Task3SnkrsItem()
-        prod['product_id'] = response.css('.product_id::text').get()
-        prod['name'] =  response.css('.name::text').get()
-        prod['brand'] = response.css('.brand::text').get()
-        prod['availability'] = response.css('.availability::text').get()
-        prod['currency'] = response.css('.price_currency_code::text').get()
-        prod['description'] = response.css('#short_description_content p::text').getall()
-        prod['old_prices'] = response.css('.list_price::text').getall()
-        prod['new_price'] = response.css('.price::attr(content)').get()
-        prod['image_urls'] = response.css('.image_url::text,.alternate_image_url::text').getall()
-        sizes = response.css('.units_container::text').getall()
+        self.response = response
+        item = SnkrsItem()
+        item['item_id'] = self.get_item_id()
+        item['name'] = self.get_name()
+        item['brand'] = self.get_brand()
+        item['availability'] = self.get_availability()
+        item['currency'] = self.get_currency()
+        item['description'] = self.get_description()
+        item['image_urls'] = self.get_images_urls()
+        item['skus'] = self.get_skus()
+
+        return item
+
+    def parse_product_urls(self, response):
+        urls_s = response.css('a.product_img_link')
+        return [response.follow(url_s, callback=self.parse_product) for url_s in urls_s]
+
+    def get_item_id(self):
+        return self.response.css('.product_id::text').get()
+
+    def get_name(self):
+        return self.response.css('.name::text').get()
+
+    def get_brand(self):
+        return self.response.css('.brand::text').get()
+
+    def get_availability(self):
+        return self.response.css('.availability::text').get()
+
+    def get_currency(self):
+        return self.response.css('.price_currency_code::text').get()
+
+    def get_description(self):
+        return self.response.css('#short_description_content p::text').getall()
+
+    def get_old_prices(self):
+        return self.response.css('.list_price::text').getall()
+
+    def get_new_price(self):
+        return self.response.css('.price::attr(content)').get()
+
+    def get_images_urls(self):
+        return self.response.css('.image_url::text,.alternate_image_url::text').getall()
+
+    def get_sizes(self):
+        sizes = self.response.css('.units_container::text').getall()
         if all(s.isspace() for s in sizes):
-            prod['sizes'] = response.css('.size_EU::text').getall()
-        else:
-            prod['sizes'] = sizes
+            self.sizes = self.response.css('.size_EU::text').getall()
 
-        if len(prod['sizes']) == 0:
-            prod['sizes'] = ['size one']
-        prod['sizes'] = [s.strip() for s in prod['sizes']]
+        if len(sizes) == 0:
+            self.sizes = ['size one']
+        sizes = [s.strip() for s in sizes]
 
-        sku = {}
-        for size in prod['sizes']:
-            sku[size] = {
+        return sizes
+
+    def get_skus(self):
+        skus = {}
+        for size in self.get_sizes():
+            skus[size] = {
                 'size': size,
-                'old_price': prod['old_prices'],
-                'new_price': prod['new_price']
+                'old_price': self.get_old_prices(),
+                'new_price': self.get_new_price()
             }
-        prod['skus'] = sku
-        print(prod)
 
-
-
-
-
+        return skus
