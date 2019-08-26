716f636407e457dd61e8484a19919e0171fa3a9a
Arslan Shakar <14051598-039@uog.edu.pk>
2019-08-26 23:25:40 +0500

    Implement Rules, LinkExtractor and Code Refactored

--- task3_snkrs/items.py
+++ task3_snkrs/items.py
@@ -1,12 +1,10 @@
-import scrapy
+from scrapy import Item, Field
 
 
-class SnkrsItem(scrapy.Item):
-    item_id = scrapy.Field()
-    name = scrapy.Field()
-    brand = scrapy.Field()
-    description = scrapy.Field()
-    availability = scrapy.Field()
-    currency = scrapy.Field()
-    image_urls = scrapy.Field()
-    skus = scrapy.Field()
+class SnkrsItem(Item):
+    item_id = Field()
+    name = Field()
+    brand = Field()
+    description = Field()
+    image_urls = Field()
+    skus = Field()
--- task3_snkrs/spiders/snkrs_spider.py
+++ task3_snkrs/spiders/snkrs_spider.py
@@ -1,35 +1,24 @@
-import scrapy
 from task3_snkrs.items import SnkrsItem
+from scrapy.spiders import CrawlSpider, Rule
+from scrapy.linkextractors import LinkExtractor
 
 
-class SnkrsSpider(scrapy.Spider):
+class SnkrsSpider(CrawlSpider):
     name = 'snkrs'
+    allowed_domains = ['snkrs.com']
     start_urls = [
-        'https://www.snkrs.com/'
+        'https://www.snkrs.com/',
     ]
-
-    def parse(self, response):
-        urls_s = response.css('ul.idparent-63 li a')
-
-        return [response.follow(url_s, callback=self.parse_pages) for url_s in urls_s]
-
-    def parse_pages(self, response):
-        yield from self.parse_product_urls(response)
-        urls_s = response.css('li.pagination_next a')
-        yield from [response.follow(url_s, callback=self.parse_pages) for url_s in urls_s]
-
-    def parse_product_urls(self, response):
-        urls_s = response.css('a.product_img_link')
-
-        return [response.follow(url_s, callback=self.parse_product) for url_s in urls_s]
+    rules = [
+        Rule(LinkExtractor(restrict_css=['ul.idparent-63 li', 'li.pagination_next'])),
+        Rule(LinkExtractor(restrict_css='a.product_img_link'), callback='parse_product'),
+    ]
 
     def parse_product(self, response):
         item = SnkrsItem()
         item['item_id'] = self.get_item_id(response)
         item['name'] = self.get_name(response)
         item['brand'] = self.get_brand(response)
-        item['availability'] = self.get_availability(response)
-        item['currency'] = self.get_currency(response)
         item['description'] = self.get_description(response)
         item['image_urls'] = self.get_images_urls(response)
         item['skus'] = self.get_skus(response)
@@ -37,53 +26,42 @@ class SnkrsSpider(scrapy.Spider):
         return item
 
     def get_item_id(self, response):
-        return response.css('.product_id::text').get()
+        return response.css('.product_id::text').getall()[0]
 
     def get_name(self, response):
-        return response.css('.name::text').get()
+        return response.css('.name::text').getall()[0]
 
     def get_brand(self, response):
-        return response.css('.brand::text').get()
-
-    def get_availability(self, response):
-        return response.css('.availability::text').get()
-
-    def get_currency(self, response):
-        return response.css('.price_currency_code::text').get()
+        return response.css('.brand::text').getall()[0]
 
     def get_description(self, response):
         return response.css('#short_description_content p::text').getall()
 
-    def get_old_prices(self, response):
-        return response.css('.list_price::text').getall()
-
-    def get_new_price(self, response):
-        return float(response.css('.price::attr(content)').get())
-
     def get_images_urls(self, response):
         return response.css('.image_url::text,.alternate_image_url::text').getall()
 
+    def get_prices(self, response):
+        return {
+            'currency': response.css('.price_currency_code::text').getall()[0],
+            'old_prices': [float(price) for price in response.css('.list_price::text').getall()],
+            'new_price': float(response.css('.price::attr(content)').getall()[0]),
+        }
+
     def get_skus(self, response):
         skus = {}
-        for raw_sku in self.get_filtered_sizes(response):
+        css = '.attribute_list li:not([class=" hidden"]) span.units_container::text'
+        sizes = self.clean(response.css(css).getall())
+        if not sizes:
+            css = '.attribute_list li:not([class=" hidden"]) span.size_EU::text'
+            sizes = self.clean(response.css(css).getall())
+        for raw_sku in sizes or ['size-one']:
             skus[raw_sku] = {
                 'size': raw_sku,
-                'old_prices': [float(price) for price in self.get_old_prices(response)],
-                'new_price': self.get_new_price(response)
+                'pricing': self.get_prices(response),
+                'availability': response.css('.availability::text').getall()[0],
             }
 
         return skus
 
-    def get_filtered_sizes(self, response):
-        sizes = response.css('.attribute_list li:not([class=" hidden"]) span.units_container::text').getall()
-        sizes = self.filter_elements(sizes)
-        if not sizes:
-            sizes = response.css('.attribute_list li:not([class=" hidden"]) span.size_EU::text').getall()
-            sizes = self.filter_elements(sizes)
-            if not sizes:
-                sizes = ['size one']
-
-        return sizes
-
-    def filter_elements(self, elements):
-        return [e.strip() for e in elements if e != ' ' and e != '']
+    def clean(self, elements):
+        return [e.strip() for e in elements if e and e.strip()]
