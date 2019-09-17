8c96850f717eebd5936b7ee429c4b37a0a76d041
ArslanShakar <37537294+ArslanShakar@users.noreply.github.com>
2019-09-17 20:06:46 +0500

    Update items.py

--- task3_snkrs/items.py
+++ task3_snkrs/items.py
@@ -1,11 +1,66 @@
-from scrapy import Field, Item
+from scrapy.item import Field, Item
 
 
-class OrsayItem(Item):
+class SnkrsItem(Item):
+    item_id = Field()
+    name = Field()
+    brand = Field()
+    description = Field()
+    image_urls = Field()
+    skus = Field()
+
+
+class LanebryantItem(Item):
     product_id = Field()
     name = Field()
     brand = Field()
     description = Field()
     image_urls = Field()
     skus = Field()
+    url = Field()
+    headers = Field()
     meta = Field()
+
+
+class NeimanItem(Item):
+    product_id = Field()
+    name = Field()
+    brand = Field()
+    description = Field()
+    image_urls = Field()
+    skus = Field()
+    url = Field()
+    headers = Field()
+    meta = Field()
+
+
+class ZillowItem(Item):
+    status = Field()
+    beds = Field()
+    baths = Field()
+    livable_sqft = Field()
+    type = Field()
+    year_built = Field()
+    heating = Field()
+    cooling = Field()
+    parking = Field()
+    lot_sqft = Field()
+    bedroom_count = Field()
+    stories = Field()
+    external_materials = Field()
+    foundation_type = Field()
+    roof = Field()
+    pool = Field()
+    garage_yn = Field()
+    hoa_fee = Field()
+    last_sold_date = Field()
+    last_sold_price = Field()
+    listed_for_sale = Field()
+    listing_price = Field()
+    rental_amount = Field()
+    parcel = Field()
+    zestimate = Field()
+    rent_zestimate = Field()
+    tax_history = Field()
+    schools = Field()
+    country_website = Field()
